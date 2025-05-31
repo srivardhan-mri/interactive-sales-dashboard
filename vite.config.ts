7e1a68b973a2f17c7dde5338903e3cf383485b82
srivardhan-mri <srivardhan@mririce.com>
2025-05-31 12:57:07 +0530

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const repositoryName = 'interactive-sales-dashboard'; // Or whatever your repo is named
     return {
+      base: mode === 'production' ? `/interactive-sales-dashboard/` : '/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
